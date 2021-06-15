f776f0c292c0c437f017a36c15b77c10ec0e2ffa
Xemerius-Sato <85961202+Sukoplay@users.noreply.github.com>
2021-06-15 23:06:43 +0200

    Update script1.js

--- JS/script1.js
+++ JS/script1.js
@@ -15,6 +15,3 @@ function twite(){
     this.classList.toggle("twitter2")
     document.querySelector(".twitt").classList.toggle("twitt2")
 }
-
-git commit --allow-empty -m "Trigger rebuild"
-git push
